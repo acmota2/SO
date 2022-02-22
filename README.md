07d9ad06f74915b2b9af428ea8574dfe095c146d
acmota2 <acmota2@gmail.com>
2022-02-22 11:18:23 +0000

    corrected cp

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # SO
+
+Estes são os exercícios dos guiões de SO
+
+Os guiões estão na pasta `./guiões`
--- mycp.c
+++ mycp.c
@@ -5,23 +5,30 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
-    int buf_s = atoi(argv[3]);
-    char *buffer = malloc(buf_s);
+    unsigned char buf[4096];
 
-    int fd1 = open(argv[1], O_CREAT | O_RDWR, 0660);
+    // para efeitos de teste:
+    // int buf_s = atoi(argv[3]);
+    // char *buffer = malloc(buf_s);
+
+    if(!argv[1] || !argv[2]) {
+        return -1;
+    }
+
+    int fd1 = open(argv[1], O_RDONLY, 0660);
     if(fd1 < 0) {
         perror("Invalid file.");
         return -1;
     }
 
-    int fd2 = open(argv[2], O_CREAT | O_RDWR, 0660);
+    int fd2 = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0660);
     if(fd2 < 0) {
         perror("Invalid file.");
         return -1;
     }
 
-    while(read(fd1, buffer, buf_s)) {
-        write(fd2, buffer, buf_s);
+    for(ssize_t i = read(fd1, buffer, buf_s); i > 0; i = read(fd1, buf, 4096)) {
+        write(fd2, buf, i);
     }
     return 0;
 }
