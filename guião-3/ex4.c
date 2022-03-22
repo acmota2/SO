330c26eb2e3db7b16f1fab52929c4c096afd7253
acmota2 <acmota2@gmail.com>
2022-03-22 15:40:10 +0000

    Added ex4 in guião-3

--- guião-3/ex4.c
+++ guião-3/ex4.c
@@ -1,21 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
 int my_system(char const* command) {
+    char args[128][128] = { 0 };
+    
+    size_t i = 0;
+    for(char *end = strchr(command, ' '), *start = strchr(command, ' ');
+        end != NULL;
+        start = end) {
+            end = strchr(command, ' ');
+            memcpy(args[i++], start, end - start);
+        }
+
+    char program[128] = { 0 };
+    memcpy(program, command, strchr(command, ' ') - command);
+
     pid_t child = fork();
     if(child == 0) {
-        int exit_status = execl("/bin/sh", "sh", "-c", command, NULL);
-        _exit(0 - (exit_status < 0));
+        int exit_status = execvp(program, args);
+        _exit(exit_status ? 42 : 0);
     } else if(child < 0) {
         perror("You're forked mate");
         return -1;
     } else {
         wait(NULL);
-        return 0;
     }
+    return 0;
 }
 
 int main(int argc, char *argv[]) {
